02a7162a3d9bf5dfbe8155f2182636e0b7d0aa72
martin.spaeth <martin.spaeth@77f07716-3dfb-11de-97fb-aff7d2a6cc54>
2009-06-25 14:18:59 +0000

    DataRowView Converter added You can now build a StoreLoadResponseWrapper with an DataView

--- trunk/ExtDirect4DotNet/customJsonConverter/DataRowConverter.cs
+++ trunk/ExtDirect4DotNet/customJsonConverter/DataRowConverter.cs
@@ -60,6 +60,61 @@ namespace ExtDirect4DotNet.customJsonConverter
         }
     }
 
+    class DataRowViewConverter : JsonConverter
+    {
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        public override void WriteJson(JsonWriter writer, object dataRow)
+        {
+            DataRowView row = dataRow as DataRowView;
+
+
+
+            // *** HACK: need to use root serializer to write the column value
+            //     should be fixed in next ver of JSON.NET with writer.Serialize(object)
+            JsonSerializer ser = new JsonSerializer();
+
+            writer.WriteStartObject();
+            int i = 0;
+            foreach (DataColumn column in row.DataView.Table.Columns)
+            {
+                writer.WritePropertyName(column.ColumnName);
+                ser.Serialize(writer, row[i]);
+                i++;
+            }
+            writer.WriteEndObject();
+
+        }
+
+
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified value type.
+        /// </summary>
+        /// <param name="valueType">Type of the value.</param>
+        /// <returns>
+        ///     <c>true</c> if this instance can convert the specified value type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type valueType)
+        {
+            return typeof(DataRowView).IsAssignableFrom(valueType);
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     class DataRowCollectionConverter : JsonConverter
     {
         /// <summary>
