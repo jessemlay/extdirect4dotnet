fcc91a556b00a215da593c0ae18f812573c7f2a6
martin.spaeth <martin.spaeth@77f07716-3dfb-11de-97fb-aff7d2a6cc54>
2009-08-13 14:52:55 +0000

    DirectExceution.cs added to wrap an executen to allow multiple status as returncode... Usefull vor Firebug A Comitted Propertie added To the Store Repsonse Wrapper that indicates if the dataset is from cash or if it is allready on the Database

---
+++ ExtDirect4DotNet/DirectExceution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtDirect4DotNet
+{
+    public class DirectExceution
+    {
+        public string jsonResponse;
+        public bool containsErrors;
+    }
+}
