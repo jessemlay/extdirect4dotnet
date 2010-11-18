399bb660d074d688ff26f856f227a1515fef1940
robolize <robolize@77f07716-3dfb-11de-97fb-aff7d2a6cc54>
2010-11-18 17:14:30 +0000

    Updated to latest version of Json.NET and included it in the lib folder.

--- branches/robolize/ExtDirect4DotNet/customJsonConverter/DataRowArrayConverter.cs
+++ branches/robolize/ExtDirect4DotNet/customJsonConverter/DataRowArrayConverter.cs
@@ -10,37 +10,26 @@ namespace ExtDirect4DotNet.customJsonConverter
     class DataRowArrayConverter : JsonConverter
     {
         /// <summary>
-        /// Writes the JSON representation of the object.
-        /// </summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        /// <param name="value">The value.</param>
-        public override void WriteJson(JsonWriter writer, object dataRow)
-        {
-            DataRowCollection rows = dataRow as DataRowCollection;
+		/// Writes the JSON representation of the object.
+		/// </summary>
+		/// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter"/> to write to.</param>
+		/// <param name="value">The value.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+			DataRowCollection rows = value as DataRowCollection;
+			writer.WriteStartArray();
+			for (int i = 0; i < rows.Count; i++) {
+				DataRow row = rows[i];
 
-            
-
-            // *** HACK: need to use root serializer to write the column value
-            //     should be fixed in next ver of JSON.NET with writer.Serialize(object)
-            JsonSerializer ser = new JsonSerializer();
-            writer.WriteStartArray();
-            for (int i = 0; i < rows.Count; i++)
-            {
-               
-
-                DataRow row = (DataRow)rows[i];
-                
-
-                writer.WriteStartObject();
-                foreach (DataColumn column in row.Table.Columns)
-                {
-                    writer.WritePropertyName(column.ColumnName);
-                    ser.Serialize(writer, row[column]);
-                }
-                writer.WriteEndObject();
-            }
-            writer.WriteEndArray();
-        }
+				writer.WriteStartObject();
+				foreach (DataColumn column in row.Table.Columns) {
+					writer.WritePropertyName(column.ColumnName);
+					serializer.Serialize(writer, row[column]);
+				}
+				writer.WriteEndObject();
+			}
+			writer.WriteEndArray();
+		}
 
         
 
@@ -57,15 +46,16 @@ namespace ExtDirect4DotNet.customJsonConverter
         }
 
         /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <returns>The object value.</returns>
-        public override object ReadJson(JsonReader reader, Type objectType)
-        {
-            throw new NotImplementedException();
-        }
+		/// Reads the JSON representation of the object.
+		/// </summary>
+		/// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader"/> to read from.</param>
+		/// <param name="objectType">Type of the object.</param>
+		/// <param name="existingValue">The existing value of object being read.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		/// <returns>The object value.</returns>
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+			throw new NotImplementedException();
+		}
     }
 
     
