fa9ad15faf3b50b5bd16d50b41c830bc52b023fa
robolize <robolize@77f07716-3dfb-11de-97fb-aff7d2a6cc54>
2010-11-18 17:14:30 +0000

    Updated to latest version of Json.NET and included it in the lib folder.

--- ExtDirect4DotNet/customJsonConverter/DataRowConverter.cs
+++ ExtDirect4DotNet/customJsonConverter/DataRowConverter.cs
@@ -11,31 +11,27 @@ namespace ExtDirect4DotNet.customJsonConverter
     class DataRowConverter : JsonConverter
     {
         /// <summary>
-        /// Writes the JSON representation of the object.
-        /// </summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        /// <param name="value">The value.</param>
-        public override void WriteJson(JsonWriter writer, object dataRow)
-        {
-            DataRow row = dataRow as DataRow;
+		/// Writes the JSON representation of the object.
+		/// </summary>
+		/// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter"/> to write to.</param>
+		/// <param name="value">The value.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+			DataRow row = value as DataRow;
 
-            
+			// *** HACK: need to use root serializer to write the column value
+			//     should be fixed in next ver of JSON.NET with writer.Serialize(object)
+			JsonSerializer ser = new JsonSerializer();
 
-            // *** HACK: need to use root serializer to write the column value
-            //     should be fixed in next ver of JSON.NET with writer.Serialize(object)
-            JsonSerializer ser = new JsonSerializer();
-            
-            ser.Converters.Add(new DataRowConverter());
-            ser.Converters.Add(new JavaScriptDateTimeConverter());
-            writer.WriteStartObject();
-            foreach (DataColumn column in row.Table.Columns)
-            {
-                writer.WritePropertyName(column.ColumnName);                
-                ser.Serialize(writer, row[column]);
-            }
-            writer.WriteEndObject();
-        
-        }
+			ser.Converters.Add(new DataRowConverter());
+			ser.Converters.Add(new JavaScriptDateTimeConverter());
+			writer.WriteStartObject();
+			foreach (DataColumn column in row.Table.Columns) {
+				writer.WritePropertyName(column.ColumnName);
+				ser.Serialize(writer, row[column]);
+			}
+			writer.WriteEndObject();
+		}
 
         
 
@@ -52,48 +48,45 @@ namespace ExtDirect4DotNet.customJsonConverter
         }
 
         /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <returns>The object value.</returns>
-        public override object ReadJson(JsonReader reader, Type objectType)
-        {
-            throw new NotImplementedException();
-        }
+		/// Reads the JSON representation of the object.
+		/// </summary>
+		/// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader"/> to read from.</param>
+		/// <param name="objectType">Type of the object.</param>
+		/// <param name="existingValue">The existing value of object being read.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		/// <returns>The object value.</returns>
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+			throw new NotImplementedException();
+		}
     }
 
     class DataRowViewConverter : JsonConverter
     {
         /// <summary>
-        /// Writes the JSON representation of the object.
-        /// </summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        /// <param name="value">The value.</param>
-        public override void WriteJson(JsonWriter writer, object dataRow)
-        {
-            DataRowView row = dataRow as DataRowView;
+		/// Writes the JSON representation of the object.
+		/// </summary>
+		/// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter"/> to write to.</param>
+		/// <param name="value">The value.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+			DataRowView row = value as DataRowView;
 
+			// *** HACK: need to use root serializer to write the column value
+			//     should be fixed in next ver of JSON.NET with writer.Serialize(object)
+			JsonSerializer ser = new JsonSerializer();
 
+			ser.Converters.Add(new JavaScriptDateTimeConverter());
 
-            // *** HACK: need to use root serializer to write the column value
-            //     should be fixed in next ver of JSON.NET with writer.Serialize(object)
-            JsonSerializer ser = new JsonSerializer();
+			writer.WriteStartObject();
+			int i = 0;
+			foreach (DataColumn column in row.DataView.Table.Columns) {
+				writer.WritePropertyName(column.ColumnName);
 
-            ser.Converters.Add(new JavaScriptDateTimeConverter());
-
-            writer.WriteStartObject();
-            int i = 0;
-            foreach (DataColumn column in row.DataView.Table.Columns)
-            {
-                writer.WritePropertyName(column.ColumnName);
-                
-                ser.Serialize(writer, row[i]);
-                i++;
-            }
-            writer.WriteEndObject();
-
-        }
+				ser.Serialize(writer, row[i]);
+				i++;
+			}
+			writer.WriteEndObject();
+		}
 
 
 
@@ -110,72 +103,60 @@ namespace ExtDirect4DotNet.customJsonConverter
         }
 
         /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <returns>The object value.</returns>
-        public override object ReadJson(JsonReader reader, Type objectType)
-        {
-            throw new NotImplementedException();
-        }
+		/// Reads the JSON representation of the object.
+		/// </summary>
+		/// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader"/> to read from.</param>
+		/// <param name="objectType">Type of the object.</param>
+		/// <param name="existingValue">The existing value of object being read.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		/// <returns>The object value.</returns>
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+			throw new NotImplementedException();
+		}
     }
 
-    class DataRowCollectionConverter : JsonConverter
-    {
-        /// <summary>
-        /// Writes the JSON representation of the object.
-        /// </summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        /// <param name="value">The value.</param>
-        public override void WriteJson(JsonWriter writer, object dataRows)
-        {
-            DataRowCollection rows = dataRows as DataRowCollection;
-            DataRowConverter converter = new DataRowConverter();
+    class DataRowCollectionConverter : JsonConverter {
+		/// <summary>
+		/// Determines whether this instance can convert the specified value type.
+		/// </summary>
+		/// <param name="valueType">Type of the value.</param>
+		/// <returns>
+		///     <c>true</c> if this instance can convert the specified value type; otherwise, <c>false</c>.
+		/// </returns>
+		public override bool CanConvert(Type valueType) {
+			return typeof (DataRowCollection).IsAssignableFrom(valueType);
+		}
 
-            
+		/// <summary>
+		/// Reads the JSON representation of the object.
+		/// </summary>
+		/// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader"/> to read from.</param>
+		/// <param name="objectType">Type of the object.</param>
+		/// <param name="existingValue">The existing value of object being read.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		/// <returns>The object value.</returns>
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+			throw new NotImplementedException();
+		}
 
+		/// <summary>
+		/// Writes the JSON representation of the object.
+		/// </summary>
+		/// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter"/> to write to.</param>
+		/// <param name="value">The value.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+			DataRowCollection rows = value as DataRowCollection;
+			DataRowConverter converter = new DataRowConverter();
 
-            // *** HACK: need to use root serializer to write the column value
-            //     should be fixed in next ver of JSON.NET with writer.Serialize(object)
-            JsonSerializer ser = new JsonSerializer();
-
-            ser.Converters.Add(new JavaScriptDateTimeConverter());
-            writer.WriteStartArray();
-            if (dataRows != null)
-            {
-                foreach (DataRow dataRow in rows)
-                {
-                    converter.WriteJson(writer, dataRow);
-                }
-            }
-            writer.WriteEndArray();
-
-        }
-
-
-
-        /// <summary>
-        /// Determines whether this instance can convert the specified value type.
-        /// </summary>
-        /// <param name="valueType">Type of the value.</param>
-        /// <returns>
-        ///     <c>true</c> if this instance can convert the specified value type; otherwise, <c>false</c>.
-        /// </returns>
-        public override bool CanConvert(Type valueType)
-        {
-            return typeof(DataRowCollection).IsAssignableFrom(valueType);
-        }
-
-        /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <returns>The object value.</returns>
-        public override object ReadJson(JsonReader reader, Type objectType)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			serializer.Converters.Add(new JavaScriptDateTimeConverter());
+			writer.WriteStartArray();
+			if (value != null) {
+				foreach (DataRow dataRow in rows) {
+					converter.WriteJson(writer, dataRow, serializer);
+				}
+			}
+			writer.WriteEndArray();
+		}
+	}
 }
